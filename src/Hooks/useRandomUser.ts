782067627b01646847ed8bdf6991253b0f9ab5dc
Tahsin Önemli <129261873+RicochetBiscuit@users.noreply.github.com>
2023-09-30 12:52:16 +0300

    customHook

---
+++ src/Hooks/useRandomUser.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export const useRandomUser = () => {
+
+const[state,setState] = useState(null);
+
+useEffect(()=>{
+    const fn = async () => {
+        const result = await fetch("https://randomuser.me/api/")
+        const json = await result.json();
+        setState(json.results);
+    };
+fn();
+
+}, [])
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
